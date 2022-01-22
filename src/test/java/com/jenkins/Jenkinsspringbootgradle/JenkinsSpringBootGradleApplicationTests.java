52fa5cc0c3fe1c5efb938547965cb50c83d74092
Ankit Bijalwan <ankit.bijalwan@tothenew.com>
2022-01-22 14:27:51 +0530

    First Commit

---
+++ src/test/java/com/jenkins/Jenkinsspringbootgradle/JenkinsSpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.Jenkinsspringbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringBootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
