56404bd021e848e91b5fc55d123db3a7b8e72842
Ankit Bijalwan <ankit.bijalwan@tothenew.com>
2022-01-27 12:54:23 +0530

    Added Test cases & deploy.sh

---
+++ src/test/java/com/jenkins/Jenkinsspringbootgradle/JenkinsSpringBootGradleApplicationTest.java
@@ -0,0 +1,23 @@
+package com.jenkins.Jenkinsspringbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class JenkinsSpringBootGradleApplicationTest {
+    @Test
+    void test1() {
+        Assert.isTrue(true, "Canary Test Failed");
+    }
+
+    @Test
+    void test2() {
+        Assert.isInstanceOf(String.class, "String");
+    }
+
+    @Test
+    void test3() {
+        Assert.isNull(null, "Object is not Null");
+    }
+}
